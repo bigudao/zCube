0660d802487ec0d96b2a434111a98077c6026031
ARPAN MONDAL <accesstoarpan@gmail.com>
2024-04-05 10:58:15 +0530

    Create constant.rs

---
+++ solana-solution/constant.rs
@@ -0,0 +1,10 @@
+use anchor_lang::prelude::*;
+
+#[constant]
+pub const USER_TAG: &[u8] = b"USER_STATE";
+
+#[constant]
+pub const zCube_TAG: &[u8] = b"ZCUBE_STATE";
+
+#[constant]
+pub const BOOK_TAG: &[u8] = b"BOOK_STATE";
